e935c91fee31c6136d6785886bf61b06785b1f1f
Aditria Pardana <aditria.pardana20@gmail.com>
2022-09-19 16:20:44 +0700

    update called setState

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class App extends React.Component{
-  render(){
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: null,
+    };
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
+      (position) => {
+        //we call update setState
+        this.setState({ lat: position.coords.latitude });
+      },
       (err) => console.log(err)
     );
+  }
 
-    return(
-      <div>
-        Latitude:
-      </div>
-    )
+  render() {
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
